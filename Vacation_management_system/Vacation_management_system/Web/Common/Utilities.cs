25831c760b803144bc180d9f451c4b51d28ef0d1
RaghavaPatnam <raghava.patnam@aguaisolutions.com>
2014-11-21 18:05:58 +0530

    Login Page Bussiness logic and Adding Utilities.cs

---
+++ Vacation_management_system/Vacation_management_system/Web/Common/Utilities.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Vacation_management_system.Web.Common
+{
+    public class Utilities
+    {
+        public string EncodePassword(string password)
+        {
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(password));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        public bool ComparePassword(string dbPassword, string hashedPassword)
+        {
+            if (dbPassword == hashedPassword)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
