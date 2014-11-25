d350043975aacfe9acb5098f9f3e983bbb9b55ee
Veena <veenakumarins1@gmail.com>
2014-11-25 15:31:11 +0530

    menu

--- Vacation_management_system/Vacation_management_system/Master/VMS.Master.designer.cs
+++ Vacation_management_system/Vacation_management_system/Master/VMS.Master.designer.cs
@@ -40,6 +40,24 @@ namespace Vacation_management_system.Master {
         protected global::System.Web.UI.WebControls.Label lblWelcome;
         
         /// <summary>
+        /// lblUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        
+        /// <summary>
+        /// menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menu1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
