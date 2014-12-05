eaf9d5862692a6f1c3bbcb866b5761e539ada6c2
veeralomati <veera.lomati@aguaisolutions.com>
2014-12-05 19:15:31 +0530

    final employee vacation and modified applyvacation

--- Vacation_management_system/Vacation_management_system/Web/EmployeeVacation/EmployeeVacation.aspx.designer.cs
+++ Vacation_management_system/Vacation_management_system/Web/EmployeeVacation/EmployeeVacation.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Vacation_management_system.Web.EmployeeVacation {
         protected global::System.Web.UI.WebControls.Button btnApprovevacation;
         
         /// <summary>
+        /// btnRejectvacation control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRejectvacation;
+        
+        /// <summary>
         /// lblEmpty control.
         /// </summary>
         /// <remarks>
@@ -38,6 +47,15 @@ namespace Vacation_management_system.Web.EmployeeVacation {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblLeaves;
+        
+        /// <summary>
+        /// lblemp_no control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblemp_no;
         
         /// <summary>
         /// GridView1 control.
